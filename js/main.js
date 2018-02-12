37bb96eb5816778ebde9b37c87c19f547ae798cc
alex_mac <alexey.rozhin90@gmail.com>
2018-02-12 13:27:25 +0300

    good dropdown for cards

--- js/main.js
+++ js/main.js
@@ -9,24 +9,22 @@
     var optionsBtn = document.querySelector('.trigger_options')
     var options = document.querySelector('.options')
     optionsBtn.addEventListener('click', function () {
+        optionsBtn.classList.toggle('trigger_open')
         optionsBtn.children[0].classList.toggle('trigger_open')
         options.classList.toggle('options-hide')
     })
 
     var optionsChildren = options.childNodes
     optionsChildren.forEach(function (value) {
-        value.addEventListener('click', function (e) {
-            if (e.target.classList.contains('options')) {
+        if (value.className === 'option') {
+            value.addEventListener('click', function (e) {
                 while (optionsBtn.hasChildNodes()) {
                     optionsBtn.removeChild(optionsBtn.lastChild)
                 }
-                optionsBtn.appendChild(e.target.cloneNode(true))
-            } else if (e.target.parentElement && e.target.parentElement.classList.contains('options')) {
-                while (optionsBtn.hasChildNodes()) {
-                    optionsBtn.removeChild(optionsBtn.lastChild)
-                }
-                optionsBtn.appendChild(e.target.parentElement.cloneNode(true))
-            }
-        })
+                optionsBtn.appendChild(value.cloneNode(true))
+                optionsBtn.classList.toggle('trigger_open')
+                options.classList.toggle('options-hide')
+            })
+        }
     })
 })();
